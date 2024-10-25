e539f0e8ef0e02b454736b9aa1d33792dadbdaf4
ForRealy <alejandrotomevalle@gmail.com>
2024-10-25 17:51:14 +0200

    Coments done

--- ObjectUse/src/ts/index.ts
+++ ObjectUse/src/ts/index.ts
@@ -7,12 +7,17 @@ function modificarBom() {
   do {
     repeat++
     // Abrimos en una ventana nueva con window.open y _blank
-    let vent = window.open('',
-                '', 
-                'width=300,height=300,');
+    let vent = window.open('', '', 'width=300,height=300,');
+    
+    // Genera un color aleatorio
     let color:string = Math.floor(Math.random()*16777215).toString(16)
     
+    // Cambiamos el fondo de las nuevas ventanas por uno aleatorio cada vez
     vent?.document.body.style.backgroundColor = "#" + color;
+    
+    // TODO Arreglar el error pero funciona todo
+
+    // Creamos un paragrafo con los datos de imput y lo añadimos a las nuevas ventanas
     const p = document.createElement("p");
     p.textContent = inputValue;
     vent?.document.body.appendChild(p);

f013f8b6f3672d7637d52d1486806859a1ebec16
ForRealy <alejandrotomevalle@gmail.com>
2024-10-30 10:06:24 +0100

    testing

--- Repaso/built/index.js
+++ Repaso/built/index.js
@@ -1,22 +1,19 @@
-"use strict";
 // Calcula si el parametro "doble" es divisible por 4
 function clic() {
     // Recoje el valor del parametro con la id "text"
-    let doble = (document.getElementById("text").value);
+    var doble = (document.getElementById("text").value);
     // Variable donde añadiremos el resultado con mayusculas intercaladas
-    let news = "";
-    // Mide el tamaño de la variable doble y lo imprime
-    let lenght = doble.length;
-    console.log(lenght);
+    var news = "";
+    // Mide el tamaño de la variable doble
+    var lenght = doble.length;
     // Condicional para comprobar si es divisible
     if (lenght % 4 == 0) {
-        console.log("Es divisible");
+        console.log("Es divisible por 4");
     }
     else
-        console.log("No es divisible");
+        console.log("No es divisible por 4");
     // Bucle para añadir parametros al string
-    let i;
-    for (i = 0; i <= lenght; i++) {
+    for (var i = 0; i <= lenght; i++) {
         // Calculas si es par o impar
         if (i % 2 == 0) {
             // Añades el caracter en posicion i en mayuscula si es impar

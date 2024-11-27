88a751283fb344df7c64bbf9c69831a24409286f
ForRealy <alejandrotomevalle@gmail.com>
2024-11-27 19:56:48 +0100

    i hate the medias

---
+++ Funcionses/index.ts
@@ -0,0 +1,39 @@
+function classificarParaules (paraules: string[]){
+
+    for (let i = 0; i < paraules.length; i++) {
+        if (paraules [i].length > 5){
+            console.log(paraules[i])
+        }
+    }
+}
+
+function analitzarTemperatures (...valores: number[]){
+    
+    const suma: number = valores.reduce((acumulador, actual) => acumulador + actual, 0);
+    const media: string = (suma / valores.length).toFixed(2);
+    
+    console.log (media, Math.max(...valores), Math.min(...valores));
+}
+
+//Funció per provar les funcions realitzades, comenteu i descomenteu una linea per separat per provar les diferents opcions
+function execucions(){
+    // Exercici 1: Classificar Paraules
+    //const paraules:string[] = ["cotxe", "avió", "ferrocarril", "vaixell", "motocicleta", "tractor"]; //Resultat: 4 elements: ferrocarril, vaixell, motocicleta, tractor
+    //const paraules:string[] = ["blau", "vermell", "taronja", "gris", "platejat"]; //Resultat: 3 elements: vermell, taronja, platejat
+    const paraules:string[] = ["gos", "gat", "elefant", "caball"]; //Resultat: 2 elements: elefant, caball
+    
+    console.log(classificarParaules(paraules));
+    
+    // Exercici 2: Analitzar Temperatures
+    analitzarTemperatures(12,4,30,2,11,26); //Resultat: Mitjana 14.17, Màxima 30, Mínima 2 
+    analitzarTemperatures(9,15,7,5,8); //Resultat: Mitjana 8.80, Màxima 15, Mínima 5 
+    analitzarTemperatures(19,20,21); //Resultat: Mitjana 20.00, Màxima 21, Mínima 19 
+
+    // Exercici 3: Enviar Missatges
+    //enviarMissatges("test@mail.com", "Demano informació sobre aquest curs"); //Resultat: Missatge enviat a l'email, contingut: Demano informació sobre aquest curs
+    //enviarMissatges("test@mail.com", "Demano informació sobre aquest curs", "Salutacions"); //Resultat: Missatge enviat a l'email, contingut: Salutacions Demano informació sobre aquest curs
+    //enviarMissatges(985421122, "Demano informació sobre aquest curs"); //Resultat (alert): Missatge enviat al telèfon, contingut: Demano informació sobre aquest curs
+    //enviarMissatges(985421122, "Demano informació sobre aquest curs", "Salutacions"); //Resultat (alert): Missatge enviat al telèfon, contingut: Salutacions Demano informació sobre aquest curs
+    //enviarMissatges("error", "error"); //Resultat: L'email no té un format vàlid.
+}
+

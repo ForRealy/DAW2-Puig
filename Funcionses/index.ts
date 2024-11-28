431465d09f35dad4cc4800f2096a05a4a6496096
ForRealy <alejandrotomevalle@gmail.com>
2024-11-28 19:10:51 +0100

    som debuging

--- Funcionses/index.ts
+++ Funcionses/index.ts
@@ -55,19 +55,19 @@ function execucions(){
     console.log(classificarParaules(paraules));
     
     // Exercici 2: Analitzar Temperatures
-    //analitzarTemperatures(12,4,30,2,11,26);
+    analitzarTemperatures(12,4,30,2,11,26);
     //Resultat: Mitjana 14.17, Màxima 30, Mínima 2 
     
-    //analitzarTemperatures(9,15,7,5,8);
+    analitzarTemperatures(9,15,7,5,8);
     //Resultat: Mitjana 8.80, Màxima 15, Mínima 5 
     
     analitzarTemperatures(19,20,21); 
     //Resultat: Mitjana 20.00, Màxima 21, Mínima 19 
 
     // Exercici 3: Enviar Missatges
-    enviarMissatges("test@mail.com", "Demano informació sobre aquest curs"); //Resultat: Missatge enviat a l'email, contingut: Demano informació sobre aquest curs
-    //enviarMissatges("test@mail.com", "Demano informació sobre aquest curs", "Salutacions"); //Resultat: Missatge enviat a l'email, contingut: Salutacions Demano informació sobre aquest curs
-    //enviarMissatges(985421122, "Demano informació sobre aquest curs"); //Resultat (alert): Missatge enviat al telèfon, contingut: Demano informació sobre aquest curs
+    //enviarMissatges("test@mail.com", "Demano informació sobre aquest curs"); //Resultat: Missatge enviat a l'email, contingut: Demano informació sobre aquest curs
+    enviarMissatges("test@mail.com", "Demano informació sobre aquest curs", "Salutacions"); //Resultat: Missatge enviat a l'email, contingut: Salutacions Demano informació sobre aquest curs
+    enviarMissatges(985421122, "Demano informació sobre aquest curs"); //Resultat (alert): Missatge enviat al telèfon, contingut: Demano informació sobre aquest curs
     //enviarMissatges(985421122, "Demano informació sobre aquest curs", "Salutacions"); //Resultat (alert): Missatge enviat al telèfon, contingut: Salutacions Demano informació sobre aquest curs
     //enviarMissatges("error", "error"); //Resultat: L'email no té un format vàlid.
 }
